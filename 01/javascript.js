85accd244d9f6dfce97065885f3e06a6af9fa36c
Pedro Martins <89556415+martins-pedro@users.noreply.github.com>
2022-12-19 07:13:01 -0300

    Primeiros scripts

---
+++ 01/javascript.js
@@ -0,0 +1,13 @@
+let idade = 20;
+console.log(idade)
+
+let nome = "Pedro";
+console.log(nome);
+
+let altura = 180;
+console.log(altura + "cm");
+
+console.log(`Seu nome é ${nome}, você tem ${idade} anos de idade e você tem ${altura} de altura.`)
+
+const valorIngressoAdulto = 20;
+console.log(`O valor para o ingresso adulto é de ${valorIngressoAdulto} reais`);
