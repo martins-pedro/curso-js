6d91f7d7be5cafe41dd909549ebfc85855efe0a2
Pedro Martins <89556415+martins-pedro@users.noreply.github.com>
2022-12-20 06:23:20 -0300

    arrays

---
+++ arrays/javascript.js
@@ -0,0 +1,7 @@
+let pedroMartins = ['Pedro Martins', 20 + ' Anos', 1.8 + 'm de altura'];
+
+console.log(pedroMartins[0]);
+
+let giovannaCorreia = ['Giovanna Correia', 18 + ' Anos', 1.63 + 'm de altura'];
+
+console.log(giovannaCorreia);
