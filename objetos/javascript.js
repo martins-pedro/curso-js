c2f55b1827ff837ed43a483b2e6aa606d3cddf5f
Pedro Martins <89556415+martins-pedro@users.noreply.github.com>
2022-12-20 06:19:09 -0300

    JS

---
+++ objetos/javascript.js
@@ -0,0 +1,21 @@
+let pessoa = {
+    nome: 'Pedro',
+    idade: 20,
+    altura: 1.80 + 'm',
+}
+
+let carro = {
+    marca: 'ford',
+    ano: 1999,
+    modelo: 'Mustang'
+};
+
+let perifericos = {
+    mouse: 'Pichau',
+    teclado: 'ReDragon',
+    monitor: 'AOC ' + 144 + 'Hz'
+};
+
+console.log(pessoa);
+console.log(carro);
+console.log(perifericos);
